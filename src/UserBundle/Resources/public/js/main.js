92410e18f09ecb65aed55c925bc73f64f6d36be2
Tim <tim1997@hotmail.fr>
2016-08-01 14:53:24 +0200

    résolution bug date

--- src/UserBundle/Resources/public/js/main.js
+++ src/UserBundle/Resources/public/js/main.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
 
 $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
-    selectYears: 150 // Creates a dropdown of 15 years to control year
-    
+    selectYears: 50, // Creates a dropdown of 15 years to control year
+    max: true,
+    format: 'dd/mm/yyyy'
 });
